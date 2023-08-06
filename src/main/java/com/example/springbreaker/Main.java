aca4f016900fdb1d8ed6a9d0c7321583dc7fd55a
kstrinadka <k.strinadka@gmail.com>
2023-08-06 12:32:30 +0700

    Заработало создание бина через context.xml

---
+++ src/main/java/com/example/springbreaker/Main.java
@@ -0,0 +1,13 @@
+package com.example.springbreaker;
+
+import com.example.springbreaker.quoters.Quoter;
+import com.example.springbreaker.quoters.TerminatorQuoter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(TerminatorQuoter.class).sayQuote();
+    }
+}
